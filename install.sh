f88bb44e45083553940e673a54a4c9fb80e72fe4
dopaemon <polarisdp@gmail.com>
2023-01-25 17:53:41 +0000

    auto config yes
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- install.sh
+++ install.sh
@@ -84,7 +84,7 @@ install_base() {
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
         apt-get update
-        apt-get install docker-ce docker-ce-cli containerd.io
+        apt-get install docker-ce docker-ce-cli containerd.io -y
         systemctl enable docker.service
     fi
 }
