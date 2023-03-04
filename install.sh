127bb2c97e40e75a6533e95c68633bb06ee4a559
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2023-03-05 00:27:50 +0700

    Fix for debian

--- install.sh
+++ install.sh
@@ -83,8 +83,8 @@ install_base() {
     else
         apt update -y
         apt install wget ufw curl unzip tar cron git socat ca-certificates gnupg lsb-release -y
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        curl -fsSL https://download.docker.com/linux/${release}/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${release} $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
         apt-get update
         apt-get install docker-ce docker-ce-cli containerd.io -y
         systemctl enable docker.service
