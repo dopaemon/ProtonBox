a67804e2ab4de30c8ea3315e27d78878d1676e4f
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2023-03-07 15:30:22 +0700

    install: Auto Say Yes

--- install.sh
+++ install.sh
@@ -83,7 +83,7 @@ install_base() {
     else
         apt update -y
         apt install wget ufw tmux curl unzip tar cron git socat ca-certificates gnupg lsb-release -y
-        curl -fsSL https://download.docker.com/linux/${release}/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+        curl -fsSL https://download.docker.com/linux/${release}/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${release} $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
         apt-get update
         apt-get install docker-ce docker-ce-cli containerd.io -y
